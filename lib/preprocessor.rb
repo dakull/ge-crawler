db3ba3bbbb1857f34f4318527137b83810c97c6e
unknown <dakull_2@.(none)>
2011-05-03 00:20:38 +0300

    page quality

--- lib/preprocessor.rb
+++ lib/preprocessor.rb
@@ -4,6 +4,7 @@
 #
 require 'open-uri'
 require 'nokogiri'
+require 'loofah'
 require 'uriio'
 
 class Preprocessor
@@ -33,42 +34,41 @@ class Preprocessor
       job = Job.find_by_name(@search_item)
       
       doc = get_uri(@uri_address+@search_item)
-      # afiseaza titlul rezultatelor si href-urile ce vor fi procesate cu threaduri
+      # preia primele pagini
       links_to_scan = []
       doc.css('h3.r > a.l').each do |link|
         links_to_scan << link['href']
-        # puts link['href']
       end
-
-      # creaza threadurile si face mici prelucrari per fiecare link
-      threads = []
+      
+      # threads = []
       results = []
       
       links_to_scan.each_with_index do |link,index|
+        page_quality = 0
         unless link.include? 'https'
-          threads << Thread.new do
+          #threads << Thread.new do
             puts "|--> " + link
             doc_child = get_uri(link)
-            unless doc_child.at_css('h1') == nil then
-              results[index] = doc_child.at_css('h1').text.strip
-              puts " -->" + doc_child.at_css('h1').text.strip
           
-              related_links = doc_child.xpath('//a[contains(text(), "'+@search_item+'")]')
-              puts "  --> nr linkuri : " + related_links.count.to_s
-              
-              related_links.css('a').each do |inner_link|
-                puts "   --> " + inner_link.content
-              end
-              
+            related_links = doc_child.xpath('//a[contains(text(), "'+@search_item+'")]')
+            puts "  --> nr linkuri : " + related_links.count.to_s
+            
+            related_links.css('a').each do |inner_link|
+              puts "   --> " + inner_link.content
+              buff = get_uri(link)
+              no_of_occurences = buff.xpath('count(//*[contains(text(), "'+@search_item+'")])').to_i
+              puts "   --> NR de Aparente : " + no_of_occurences.to_s
+              page_quality = page_quality + no_of_occurences
             end
-          end
+            puts "   --> ** Page Quality : " + page_quality.to_s
+          #end
         end
       end
       
       # join
-      threads.each do |thread|
-        thread.join
-      end
+      # threads.each do |thread|
+      #   thread.join
+      # end
       
       # jobul este gata save the data
       # ATENTIE !
