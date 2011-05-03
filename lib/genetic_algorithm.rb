c62e9c9e5f95934805e033547a68eb8aa2e442b9
dakull <dakull3k@yahoo.com>
2011-05-03 17:40:36 +0300

    added GA module

--- lib/genetic_algorithm.rb
+++ lib/genetic_algorithm.rb
@@ -1,7 +1,40 @@
-class GeneticAlgorithm
-  
-  attr_accessor :search_terms, :iterations
-  
-  
+module GA
+
+  class GeneticAlgorithm
+    
+    attr_accessor :search_terms, :iterations, :algorithm
+    
+    def initialize(iterations,algorithm)
+      @iterations = iterations
+      @algorithm = algorithm
+    end
+    
+    def run_algorithm
+      @algorithm.call(self)
+    end
+    
+  end
+
+  # testing grounds : 
+  def test_run
+    ge_mark_i = Proc.new { |context|
+      for i in 1..context.iterations do
+        puts i
+      end
+    }
+    
+    buff = GeneticAlgorithm.new 40, ge_mark_i
+    buff.iterations = 40
+    buff.run_algorithm
+  end
   
 end
+
+
+class Test
+  include GA
+end
+
+Test.new.test_run
+
+# end module GA  
